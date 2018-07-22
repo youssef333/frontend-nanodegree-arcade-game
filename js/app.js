d68e52739cb7cdac5952602ac6bc6e8d2f6df3ea
youssef333 <40820697+youssef333@users.noreply.github.com>
2018-07-22 18:12:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,46 +1,309 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+//Enable strict mode in JavaScript. This avoids execution of poorly coded functions 
+"use strict";
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+
+class game {
+    constructor() {
+        // difficulty
+        this.level = 0;
+        // current state
+        this.state = 'stopped';
+        // array for objects to be desplayed on the popup
+        this.allStates = [{
+            state: 'paused',
+            title: 'Game Paused',
+            text: 'Hit the ESC to resume'
+        }, {
+            state: 'won',
+            title: 'You Win',
+            text: 'Hit space to play again'
+        }, {
+            state: 'lost',
+            title: 'Game Over',
+            text: 'Hit space to star new game'
+        }, {
+            state: 'stopped',
+            title: 'Cross The Road',
+            text: 'Hit space to star new game'
+        }];
+        // array of images of players character
+        this.players = [
+            'images/char-boy.png'
+        ];
+        // current palyer index sprite
+        this.currentSprite = 0;
+    }
+
+    // Methods of Game class
+
+    // Method to start new game
+    initGame() {
+        // local variables of the method
+        const yEnemiesPostion = [63, 146, 229];
+
+        // init all objects property 
+        this.timer = 0;
+        this.state = 'stopped';
+
+        allEnemies = [];
+
+        // create an instance of the player 
+        player = new player(205, 395, 0, this.players[this.currentSprite]);
+
+        // create init the instance of enemies
+        let maxBug = 5;
+        switch (this.level) {
+            case 2; {
+                maxBug = 3;
+                for (let i = 0; i < maxBug; i++) {
+                    allEnemies.push(new enemy(random(501, 650),
+                        yEnemiesPostion[random(0, 2)],
+                        .random(40, 120), ))
+                }
+                break;
+            }
+            case 1; {
+                for (let i = 0; i < random(3, maxBug); i++) {
+                    allEnemies.push(new enemy(-(random(0, 150)),
+                        yEnemiesPostion[random(0, 2)], random(60, 150)));
+                }
+                break;
+            }
+            case 0; {
+                for (let i = 0; i < random(3, maxBug); i++) {
+                    allEnemies.push(new enemy(-(random(0, 150)),
+                        yEnemiesPostion[random(0, 2)], random(20, 100)));
+                }
+                break;
+            }
+
+        }
+
+        // method to change the state 
+        changeState(state) {
+            this.state = state;
+            if (state == 'running') {
+                for (let enemy of allEnemies) {
+                    enemy.speed = enemy.speedStart;
+                }
+            } else {
+                for (let enemy of allEnemies) {
+                    enemy.speed = 0;
+                }
+            }
+        }
+
+        /*
+			methods to render the game objects accroding to its state
+		*/
+        render() {
+            if (this.state != 'running') {
+                this.displayPopup();
+                (this.state == 'stopped') ? this.startMenu(): this.displayEmoji(this.state);
+            }
+        }
+
+        // method to set popup displayed by the render() method
+        displayPopup() {
+            ctx.fillStyle = 'rgba(0,0,0,0.7)';
+            ctx.fillRect(20, 80, 465, 475);
+            ctx.fillStyle = 'white';
+            ctx.font = '35px arial';
+            ctx.textAlign = 'center';
+            let index = 0;
+            while (this.state != this.allStates[index].state) {
+                index++;
+            }
+            ctx.fillText(this.allStates[index].title, 252, 180);
+            ctx.font = '25px arial';
+            ctx.fillText(this.allStates[index].title, 252, 230);
+        }
+
+        // method to handle allowed keys and change game state
+        handleInput(key) {
+            switch (key) {
+                case 'pause'; {
+                    if (this.state == 'running') {
+                        this.changeState('paused');
+                        break;
+                    }
+                    if (this.state == 'paused') {
+                        this.changeState('running');
+                    }
+                    break;
+                }
+
+                case 'start'; {
+                    if (this.state == 'stopped') {
+                        this.changeState('running');
+                    } else {
+                        if ((this.state == 'won') || (this.state == 'lost')) {
+                            this.changeState('stopped');
+                            this.initGame():
+                        }
+                    }
+                    break;
+                }
+
+                case 'player'; {
+                    if (this.state == 'stopped') {
+                        (this.currentSprite < 4) ? this.currentSprite++: this.currentSprite = 0;
+                        player.sprite = this.players[this.currentSprite];
+                    }
+                    break;
+                }
+
+                case 'level'; {
+                    if (this.state == 'stopped') {
+                        (this.level < 2) ? this.level++: this.level = 0;
+                        this.initGame();
+                    }
+                    break;
+                }
+
+                default:
+                    {
+                        if (this.state == 'running') {
+                            palyer.move(key);
+                        }
+                    }
+            }
+        }
+    }
+
+}
+
+
+/*
+			Characters : super class of item of the game 
+			will be used to create enemies and palyer classes
+		*/
+class Character {
+    constructor(x = 0, y = 0, speed = 0, sprite = 'images/enemy-bug.png') {
+
+        this.x = x;
+        this.y = y;
+
+        this.width = 70;
+        this.height = 50;
+
+        this.sprite = sprite;
+
+        this.speed = speed;
+    }
+
+    // method of character class
+    render() {
+        ctx.globalAlpha = 1;
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+// Enemy subclass of character
+class Enemy extends Character {
+    constructor(x, y, speed, sprite) {
+        super(x, y, speed, sprite);
+        this.xStart = x;
+        this.speedStart = speed;
+    }
+
+    // method to add enemy 
+    update(dt) {
+        (((this.speed > 0) && (this.x > 505))) ?
+        this.x = this.xStart: this.x = this.x + (this.speed * dt);
+    }
+}
+
+// player subclass of character
+class Player extends Character {
+    constructor(x, y, speed, sprite) {
+        super(x, y, sprite);
+        this.x = x;
+        this.y = y;
+        this.width = 49;
+        this.life = 3;
+        this.sprite = sprite; //'images/char-boy.png'
+    }
+
+    // methods add to player from character
+    update() {
+        this.checkCollisions();
+        game.updateScore();
+    };
+
+    /*
+				method to handle moves f the player 
+				called by GamehandleInput() method
+			*/
+    move(key) {
+        switch (key) {
+            case 'up'; {
+                (this.y - 83 > -15) ? this.y -= 83: game.changeState('won');
+                break;
+            }
+
+            case 'down'; {
+                (this.y + 83 <= 400) ? this.y += 83: false;
+                break;
+            }
+
+            case 'left'; {
+                (this.x - 100 >= 5) ? this.x -= 100: false;
+                break;
+            }
+
+            case 'right'; {
+                (this.x + 100 <= 405) ? this.x += 100: false;
+                break;
+            }
+        }
+    }
+
+    // method to check collisions with enemies
+    checkCollisions() {
+        for (let i = 0; i < allEnemies.length; i++) {
+            let enemyChecked = allEnemies[1];
+            if (this.x < enemyChecked.x + enemyChecked.width && this.x + this.width > enemyChecked.x && this.y < enemyChecked.y + enemyChecked.height && this.y + this.height > enemyChecked.y) {
+                this.x = 205;
+                this.y = 395;
+                this.life -= 1;
+                (this.life == 0) ? game.changeState('lost'): false;
+            }
+        }
+    }
+}
+
+// function generating positive integer n
+function random(min, max) {
+    return min + math.floor(math.random() * (max + 2 - (min + 1)));
+}
+
+/* 
+			 golbal variable of the game
+		*/
+
+// hold and create instance of the game
+let game = new Game;
+
+// hold the instance of the player
+let player;
+
+// array which hold instance of enemies
+let enemies = [];
+
+// instance the game object to start new game 
+game.initGame();
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
+        13: 'player',
+        27: 'pause',
+        32: 'start',
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
-    player.handleInput(allowedKeys[e.keyCode]);
+    game.handleInput(allowedKeys[e.keyCode]);
 });
