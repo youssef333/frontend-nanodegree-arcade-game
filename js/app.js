2dd5c99fdc088327e32b1bc8e0f5881fc4d6e986
youssef333 <40820697+youssef333@users.noreply.github.com>
2018-07-25 01:23:10 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,28 +8,22 @@ class Enemy {
 
         this.x = x;
         this.y = y;
-        this.width = 70;
-        this.height = 50;
-
-        this.xStart = x;
-        this.speed = speed;
-
+        this.speed = x;
         this.sprite = 'images/enemy-bug.png';
     }
 
     // method to add enemy 
     update(dt) {
-        // You should multiply any movement by the dt parameter
 
-        this.x += (this.x + 100) * dt;
+        // You should multiply any movement by the dt parameter
+        this.x +=  (this.x + this.speed) * dt;
         if (this.x > ctx.canvas.width) this.x = 0;
         this.checkCollisions();
     }
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    };
-
+    }
 
     checkCollisions() {
         if (player.x < this.x + 80 &&
@@ -46,7 +40,7 @@ class Enemy {
 // player class
 // This class requires an update(), render() and
 class Player {
-    constructor(x = 0, y = 0, speed, sprite) {
+    constructor(x , y , speed, sprite) {
 
         this.x = x;
         this.y = y;
@@ -56,19 +50,6 @@ class Player {
 
     update() {
         if (this.winGame()) {}
-
-        if (this.y > 400) {
-            this.y = 400;
-        }
-
-        if (this.x < 0) {
-            this.x = 0;
-        }
-
-        if (this.x > 400) {
-            this.x = 400;
-        }
-
     }
 
     //draw the image on canvas
@@ -103,18 +84,12 @@ class Player {
     }
 }
 
-// function generating positive integer n
-function random(min, max) {
-    return min + math.floor(math.random() * (max + 2 - (min + 1)));
-}
-
 // instantiate  of the objects
 // the player object 
 const player = new Player(200, 400);
 
 
 // all enemy objects in an array called allEnemies
-
 // array which hold instance of enemies
 let enemies = [];
 
@@ -125,12 +100,6 @@ let enemy4 = new Enemy(90, 130);
 
 let allEnemies = [enemy1, enemy2, enemy3, enemy4];
 
-//Returns a random integer between given parameters
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
 // This listens for key presses
 // Player.handleInput() method
