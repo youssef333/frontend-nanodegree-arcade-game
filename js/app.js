6ca45f36a118db4d61721945fcf64ddd3b071158
youssef333 <40820697+youssef333@users.noreply.github.com>
2018-07-24 20:31:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,186 +2,9 @@
 "use strict";
 
 
-
-
-class game {
-    constructor() {
-        // difficulty
-        this.level = 0;
-        // current state
-        this.state = 'stopped';
-        // array for objects to be desplayed on the popup
-        this.allStates = [{
-            state: 'paused',
-            title: 'Game Paused',
-            text: 'Hit the ESC to resume'
-        }, {
-            state: 'won',
-            title: 'You Win',
-            text: 'Hit space to play again'
-        }, {
-            state: 'lost',
-            title: 'Game Over',
-            text: 'Hit space to star new game'
-        }, {
-            state: 'stopped',
-            title: 'Cross The Road',
-            text: 'Hit space to star new game'
-        }];
-        // array of images of players character
-        this.players = [
-            'images/char-boy.png'
-        ];
-        // current palyer index sprite
-        this.currentSprite = 0;
-    }
-
-    // Methods of Game class
-
-    // Method to start new game
-    initGame() {
-        // local variables of the method
-        const yEnemiesPostion = [63, 146, 229];
-
-        // init all objects property 
-        this.timer = 0;
-        this.state = 'stopped';
-
-        allEnemies = [];
-
-        // create an instance of the player 
-        player = new player(205, 395, 0, this.players[this.currentSprite]);
-
-        // create init the instance of enemies
-        let maxBug = 5;
-        switch (this.level) {
-            case 2; {
-                maxBug = 3;
-                for (let i = 0; i < maxBug; i++) {
-                    allEnemies.push(new enemy(random(501, 650),
-                        yEnemiesPostion[random(0, 2)],
-                        .random(40, 120), ))
-                }
-                break;
-            }
-            case 1; {
-                for (let i = 0; i < random(3, maxBug); i++) {
-                    allEnemies.push(new enemy(-(random(0, 150)),
-                        yEnemiesPostion[random(0, 2)], random(60, 150)));
-                }
-                break;
-            }
-            case 0; {
-                for (let i = 0; i < random(3, maxBug); i++) {
-                    allEnemies.push(new enemy(-(random(0, 150)),
-                        yEnemiesPostion[random(0, 2)], random(20, 100)));
-                }
-                break;
-            }
-
-        }
-
-        // method to change the state 
-        changeState(state) {
-            this.state = state;
-            if (state == 'running') {
-                for (let enemy of allEnemies) {
-                    enemy.speed = enemy.speedStart;
-                }
-            } else {
-                for (let enemy of allEnemies) {
-                    enemy.speed = 0;
-                }
-            }
-        }
-
-        /*
-			methods to render the game objects accroding to its state
-		*/
-        render() {
-            if (this.state != 'running') {
-                this.displayPopup();
-                (this.state == 'stopped') ? this.startMenu(): this.displayEmoji(this.state);
-            }
-        }
-
-        // method to set popup displayed by the render() method
-        displayPopup() {
-            ctx.fillStyle = 'rgba(0,0,0,0.7)';
-            ctx.fillRect(20, 80, 465, 475);
-            ctx.fillStyle = 'white';
-            ctx.font = '35px arial';
-            ctx.textAlign = 'center';
-            let index = 0;
-            while (this.state != this.allStates[index].state) {
-                index++;
-            }
-            ctx.fillText(this.allStates[index].title, 252, 180);
-            ctx.font = '25px arial';
-            ctx.fillText(this.allStates[index].title, 252, 230);
-        }
-
-        // method to handle allowed keys and change game state
-        handleInput(key) {
-            switch (key) {
-                case 'pause'; {
-                    if (this.state == 'running') {
-                        this.changeState('paused');
-                        break;
-                    }
-                    if (this.state == 'paused') {
-                        this.changeState('running');
-                    }
-                    break;
-                }
-
-                case 'start'; {
-                    if (this.state == 'stopped') {
-                        this.changeState('running');
-                    } else {
-                        if ((this.state == 'won') || (this.state == 'lost')) {
-                            this.changeState('stopped');
-                            this.initGame():
-                        }
-                    }
-                    break;
-                }
-
-                case 'player'; {
-                    if (this.state == 'stopped') {
-                        (this.currentSprite < 4) ? this.currentSprite++: this.currentSprite = 0;
-                        player.sprite = this.players[this.currentSprite];
-                    }
-                    break;
-                }
-
-                case 'level'; {
-                    if (this.state == 'stopped') {
-                        (this.level < 2) ? this.level++: this.level = 0;
-                        this.initGame();
-                    }
-                    break;
-                }
-
-                default:
-                    {
-                        if (this.state == 'running') {
-                            palyer.move(key);
-                        }
-                    }
-            }
-        }
-    }
-
-}
-
-
-/*
-			Characters : super class of item of the game 
-			will be used to create enemies and palyer classes
-		*/
-class Character {
-    constructor(x = 0, y = 0, speed = 0, sprite = 'images/enemy-bug.png') {
+// Enemy class
+class Enemy {
+    constructor(x , y, speed , sprite) {
 
         this.x = x;
         this.y = y;
@@ -189,23 +12,9 @@ class Character {
         this.width = 70;
         this.height = 50;
 
-        this.sprite = sprite;
-
-        this.speed = speed;
-    }
-
-    // method of character class
-    render() {
-        ctx.globalAlpha = 1;
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-}
-// Enemy subclass of character
-class Enemy extends Character {
-    constructor(x, y, speed, sprite) {
-        super(x, y, speed, sprite);
+        this.sprite = 'images/enemy-bug.png';
         this.xStart = x;
-        this.speedStart = speed;
+        this.speed = speed;
     }
 
     // method to add enemy 
@@ -213,97 +22,101 @@ class Enemy extends Character {
         (((this.speed > 0) && (this.x > 505))) ?
         this.x = this.xStart: this.x = this.x + (this.speed * dt);
     }
+    render () {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
 }
 
-// player subclass of character
-class Player extends Character {
-    constructor(x, y, speed, sprite) {
-        super(x, y, sprite);
+
+// player class
+// This class requires an update(), render() and
+class Player {
+    constructor(x =0, y=0, speed , sprite) {
+
         this.x = x;
         this.y = y;
-        this.width = 49;
-        this.life = 3;
-        this.sprite = sprite; //'images/char-boy.png'
+        this.sprite = 'images/char-boy.png';
+        this.level = 1;
     }
 
-    // methods add to player from character
-    update() {
-        this.checkCollisions();
-        game.updateScore();
-    };
+    update() { 
+        if(this.winGame()) {
+        }
 
-    /*
-				method to handle moves f the player 
-				called by GamehandleInput() method
-			*/
-    move(key) {
-        switch (key) {
-            case 'up'; {
-                (this.y - 83 > -15) ? this.y -= 83: game.changeState('won');
-                break;
-            }
+        if(this.y > 400) {
+            this.y = 400;
+        }
 
-            case 'down'; {
-                (this.y + 83 <= 400) ? this.y += 83: false;
-                break;
-            }
+        if(this.x < 0){
+            this.x = 0;
+        }
 
-            case 'left'; {
-                (this.x - 100 >= 5) ? this.x -= 100: false;
-                break;
-            }
-
-            case 'right'; {
-                (this.x + 100 <= 405) ? this.x += 100: false;
-                break;
-            }
+        if(this.x > 400){
+            this.x = 400;
         }
     }
 
-    // method to check collisions with enemies
-    checkCollisions() {
-        for (let i = 0; i < allEnemies.length; i++) {
-            let enemyChecked = allEnemies[1];
-            if (this.x < enemyChecked.x + enemyChecked.width && this.x + this.width > enemyChecked.x && this.y < enemyChecked.y + enemyChecked.height && this.y + this.height > enemyChecked.y) {
-                this.x = 205;
-                this.y = 395;
-                this.life -= 1;
-                (this.life == 0) ? game.changeState('lost'): false;
-            }
+    //draw the image on canvas
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    //handles the move of player based on keypress
+    handleInput(keypress) {
+        switch (keypress) {
+            case 'left':
+                this.x -= 100;
+                break;
+            case 'right':
+                this.x += 100;
+                break;
+            case 'up':
+                this.y -= 83;
+                break;
+            case 'down':
+                this.y += 83;
+        }
+    }
+
+    //if player reach the water block reset the position to start
+    winGame(){
+        if(this.y < -50){
+            this.x = 200;
+            this.y = 400;
+            return true;
         }
     }
 }
 
-// function generating positive integer n
-function random(min, max) {
-    return min + math.floor(math.random() * (max + 2 - (min + 1)));
-}
+// instantiate  of the objects
+// the player object 
+const player = new Player(200, 400);
 
-/* 
-			 golbal variable of the game
-		*/
+// all enemy objects in an array called allEnemies
+const allEnemies = [];
 
-// hold and create instance of the game
-let game = new Game;
-
-// hold the instance of the player
-let player;
+//all possible positions for an enemy
+const rows = [64, 147, 230];
 
 // array which hold instance of enemies
 let enemies = [];
 
-// instance the game object to start new game 
-game.initGame();
+//Returns a random integer between given parameters
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
 
+// This listens for key presses
+// Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
-        13: 'player',
-        27: 'pause',
-        32: 'start',
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-    game.handleInput(allowedKeys[e.keyCode]);
+
+    player.handleInput(allowedKeys[e.keyCode]);
 });
